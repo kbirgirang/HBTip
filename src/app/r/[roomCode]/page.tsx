21d37abf598489749afcc7e0979c4668ba6ab038
Kari <kari@raftakn.is>
2026-01-02 14:36:36 +0000

    cursor að laga- user geti verið í mörgum deildum- hafa hnapp til að stökkva á milli deilda

--- src/app/r/[roomCode]/page.tsx
+++ src/app/r/[roomCode]/page.tsx
@@ -345,7 +345,7 @@ export default function RoomPage() {
                   <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                   <path d="M16 3.13a4 4 0 0 1 0 7.75" />
                 </svg>
-                <span>{loadingRooms ? "Hleð..." : `Skipta deild (${myRooms.length})`}</span>
+                <span>{loadingRooms ? "Hleð..." : `Þínar deildir (${myRooms.length})`}</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"

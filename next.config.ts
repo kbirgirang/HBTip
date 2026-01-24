5ff4dc65be496abd0272093e895d3ffdca16e957
Kari <kari@raftakn.is>
2026-01-24 10:16:53 +0000

    fánavesen

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: process.env.NODE_ENV === 'development'
+              ? "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live; object-src 'none'; base-uri 'self';"
+              : "script-src 'self' 'unsafe-inline' https://vercel.live; object-src 'none'; base-uri 'self';"
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
